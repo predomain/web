7aa00e02636c5856db413bf1aded8fb6cdad0519
mtxjwo <jennifer@foregate.co>
2022-09-01 14:13:04 +0200

    Update; reduced totalCostBuffer by 10%, previously 15% to anticipate ETHUSD price deviations from Chainlink.

--- src/app/configurations/general.conf.ts
+++ src/app/configurations/general.conf.ts
@@ -20,7 +20,7 @@ export const generalConfigurations = {
   maxIpfsSubscriptionRetries: 5,
   maxDomainsToRegister: 50,
   maxYearsRegistration: 1000,
-  maxTotalCostBuffer: 115,
+  maxTotalCostBuffer: 105,
   defaultCurrency: 'ETH',
   enableCustomRPC: true,
   docsLink: 'https://predomain.gitbook.io/predomain-docs/',
