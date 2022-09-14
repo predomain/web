b85866c4cd38745cb3905a3b93be3c4eb58cffa2
mtxjwo <jennifer@foregate.co>
2022-09-14 22:07:58 +0200

    Update; added conditional formattig to the time-ago pipe to display time-ago only if the time is less than a year.

--- src/app/modules/pipes/time-ago.pipe.ts
+++ src/app/modules/pipes/time-ago.pipe.ts
@@ -1,16 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en';
+import { FormatTimePipe } from './format-time.pipe';
 TimeAgo.addDefaultLocale(en);
 
 @Pipe({
   name: 'timeAgo',
 })
 export class TimeAgoPipe implements PipeTransform {
+  constructor(protected formatTimePipe: FormatTimePipe) {}
+
   transform(value: string): any {
     const timeAgo = new TimeAgo('en-US');
     if (value) {
       const time = timeAgo.format(new Date(parseInt(value)));
+      if (time.toLowerCase().indexOf('year') > -1) {
+        return this.formatTimePipe.transform(value);
+      }
       return time;
     }
   }
